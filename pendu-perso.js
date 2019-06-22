622b662240a511df6290b2dd4dad14004947cab6
jessica <jessolivette@hotmail.fr>
2019-06-22 16:24:43 +0200

    resize main containers

---
+++ pendu-perso.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+
+// global variables
+
+// array for six letters words
+var words = ["TOMATE", "SORTIE", "AMICAL", "FARINE", "FUTILE", "MODULE", "TREIZE", "PATATE", "BISOUS", "CANARD"];
+// current game variable
+var game = 1;
+var score = 0;
+// get user input
+var suggestedLetter = document.getElementById("suggestedLetter").value;
+
+};
